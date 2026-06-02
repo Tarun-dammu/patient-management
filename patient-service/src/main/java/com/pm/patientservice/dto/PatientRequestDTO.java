9237d7bdcf2f748a1d5d4a4d1e4db4b7adc41a11
Tarun-dammu <63595715+Tarun-dammu@users.noreply.github.com>
2026-06-02 22:10:16 +0530

    Intial commit

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,31 @@
+package com.pm.patientservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PatientRequestDTO {
+
+    @NotBlank
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message="Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of birth is required")
+    private String dateOfBirth;
+
+    @NotNull(message = "Registered Date is required")
+    private String registeredDate;
+
+}
